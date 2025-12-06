00e76e96a1a8afa4450d7655135594b1f4dfdbb9
uprashanth222 <uprashanth222@gmail.com>
2025-12-06 18:16:10 +0530

    Added dummy alert for testing

--- Spotify clone/Spotify clone/script.js
+++ Spotify clone/Spotify clone/script.js
@@ -3,6 +3,7 @@ let gif = document.getElementById("gif");
 
 play.addEventListener("click", () => {
   //   alert("display pause is pressed");
+  // alert("just for testing github")
   play.classList.toggle("fa-play");
   play.classList.toggle("fa-pause");
   gif.classList.toggle("video");
